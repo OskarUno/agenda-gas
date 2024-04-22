cb513c0506e0061873e2b6c67dabc95c3a44f081
Oskar_Uno <oskar310cl@gmail.com>
2024-04-21 23:23:21 -0400

    V9 creacion de primera tabla

--- funciones.js
+++ funciones.js
@@ -5,3 +5,9 @@ function doGet(){
 function obtenerDatosHtml(nombre){
 return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos(){
+    let hoja = SpreadsheetApp.openById('1vD7A95G0k2rh1SU0_D8jxKLRbXHP4wQSC_B9Yyquewc').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
