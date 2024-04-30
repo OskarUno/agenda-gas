b490cc6b4934b42b75f78b77cd649bac9ac2ce1c
Oskar_Uno <oskar310cl@gmail.com>
2024-04-30 19:18:47 -0400

    V18 Boton Borrar contacto

--- funciones.js
+++ funciones.js
@@ -1,21 +1,25 @@
 const HOJA = SpreadsheetApp.openById('1vD7A95G0k2rh1SU0_D8jxKLRbXHP4wQSC_B9Yyquewc').getActiveSheet();
 
-function doGet(datos){
+function doGet(datos) {
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Scripts');
 }
 
-function doPost(datos){
-    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Scripts'); 
+function doPost(datos) {
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Scripts');
 }
 
-function obtenerDatosHtml(nombre){
-return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+function obtenerDatosHtml(nombre) {
+    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
 
-function obtenerContactos(){
+function obtenerContactos() {
     return HOJA.getDataRange().getValues();
 }
 
-function insertarContacto(nombre, apellidos, correo, telf){
+function insertarContacto(nombre, apellidos, correo, telf) {
     HOJA.appendRow([nombre, apellidos, correo, telf]);
+}
+
+function borrarContacto(numFila) {
+    HOJA.deleteRow(numFila);
 }
